94b0062819d85884e9de7fee22a3def8a088a1ac
izeye <izeye@naver.com>
2015-06-14 10:07:00 +0900

    Initial commit

---
+++ src/main/java/com/ctb/linksharer/Application.java
@@ -0,0 +1,16 @@
+package com.ctb.linksharer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by izeye on 15. 6. 14..
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
