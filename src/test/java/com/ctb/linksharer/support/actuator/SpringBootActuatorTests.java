2d783f53567f24462dc9f44098097f5f55804b4b
izeye <izeye@naver.com>
2015-06-19 00:36:44 +0900

    Apply Spring Security to Spring Boot Actuator.

--- src/test/java/com/ctb/linksharer/support/actuator/SpringBootActuatorTests.java
+++ src/test/java/com/ctb/linksharer/support/actuator/SpringBootActuatorTests.java
@@ -6,12 +6,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.TestRestTemplate;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.StringContains.containsString;
 
 /**
  * Created by izeye on 15. 6. 16..
@@ -20,6 +21,12 @@ import static org.hamcrest.core.Is.is;
 @SpringApplicationConfiguration(classes = Application.class)
 @WebIntegrationTest(randomPort = true)
 public class SpringBootActuatorTests {
+	
+	@Value("${security.user.name}")
+	String username;
+
+	@Value("${security.user.password}")
+	String password;
 
 	@Value("${local.server.port}")
 	int port;
@@ -28,17 +35,25 @@ public class SpringBootActuatorTests {
 	
 	@Before
 	public void setUp() {
-		this.restTemplate = new RestTemplate();
+		this.restTemplate = new TestRestTemplate(username, password);
 	}
 	
 	@Test
 	public void testHealth() {
-		String expected = "{\"status\":\"UP\"}";
-		
 		String url = "http://localhost:{port}/management/health";
 		
 		String response = this.restTemplate.getForObject(url, String.class, port);
-		assertThat(response, is(expected));
+		System.out.println(response);
+		assertThat(response, containsString("UP"));
+	}
+	
+	@Test
+	public void testConfigProps() {
+		String url = "http://localhost:{port}/management/configprops";
+
+		String response = this.restTemplate.getForObject(url, String.class, port);
+		System.out.println(response);
+		assertThat(response, containsString("serverProperties"));
 	}
 	
 }
